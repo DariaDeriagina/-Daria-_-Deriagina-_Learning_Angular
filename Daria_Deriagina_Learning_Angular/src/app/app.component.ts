3af6520fe5da0542bf465effbb8452b5fb9663e2
Daria Deriagina <deriagina.daria@icloud.com>
2024-09-15 10:30:10 -0400

    Create starter Angular file and completed Assignment 1

--- Daria_Deriagina_Learning_Angular/src/app/app.component.ts
+++ Daria_Deriagina_Learning_Angular/src/app/app.component.ts
@@ -6,8 +6,10 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'], // Use 'styleUrls' and pass an array
 })
 export class AppComponent {
   title = 'Daria_Deriagina_Learning_Angular';
+  age = 36; // Declare the 'age' variable
+  city = 'Windsor'; // Declare the 'city' variable
 }
