b3f15fcd513f98428ae93fd3bbb68990f27c8ad7
Daria Deriagina <w0862447@myscc.ca>
2024-09-19 19:16:58 -0400

    Creating 2 new components

---
+++ Daria_Deriagina_Learning_Angular/src/app/investment-list/investment-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-list.component.html',
+  styleUrl: './investment-list.component.scss'
+})
+export class InvestmentListComponent {
+
+}
