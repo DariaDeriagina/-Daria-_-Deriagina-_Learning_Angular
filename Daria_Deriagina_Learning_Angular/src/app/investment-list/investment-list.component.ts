720350e4ac7c873182cb7b40a20556418cfd1983
Daria Deriagina <w0862447@myscc.ca>
2024-09-26 19:19:28 -0400

    Create service and mock we will use for CRUD operations

--- Daria_Deriagina_Learning_Angular/src/app/investment-list/investment-list.component.ts
+++ Daria_Deriagina_Learning_Angular/src/app/investment-list/investment-list.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { InvestmentListItemComponent } from '../investment-list-item/investment-list-item.component';  // Import child component
 
-interface Investment {
-  name: string;
-  initialAmount: number;
-  interestRate: number;
-  duration: number;
-  compoundFrequency: string;
-  image: string;
-}
+
 @Component({
   selector: 'app-investment-list',
   standalone: true,
@@ -20,39 +13,6 @@ interface Investment {
 
 export class InvestmentListComponent {
   // Define the array with 4 content items
-  investments: Investment[] = [
-    {
-      name: 'Retirement Fund',
-      initialAmount: 10000,
-      interestRate: 5,
-      duration: 10,
-      image: 'src/app/assets/images/college.jpeg',
-      compoundFrequency: 'Annually'
-    },
-    {
-      name: 'Home Renovation Fund',
-      initialAmount: 5000,
-      interestRate: 4,
-      duration: 5,
-      image: '../assets/images/homeRenovation.jpeg',
-      compoundFrequency: 'Monthly'
-    },
-    {
-      name: 'Emergency Fund',
-      initialAmount: 15000,
-      interestRate: 3,
-      duration: 3,
-      image:'../assets/images/emergency.jpeg',
-      compoundFrequency: 'Quarterly'
-    },
-    {
-      name: 'College Savings Plan',
-      initialAmount: 20000,
-      interestRate: 6,
-      duration: 7,
-      image: 'src/app/assets/images/college.jpeg',
-      compoundFrequency: 'Bi-Annually'
 
-    }
-  ];
 }
+
