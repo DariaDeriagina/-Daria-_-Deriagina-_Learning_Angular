c05c796ab844fa44b6faf2fc79b1c4d8edace1d3
Daria Deriagina <w0862447@myscc.ca>
2024-10-02 21:11:35 -0400

    Implemented steps 10, 11, and 12: Added InvestmentListItemComponent, used InvestmentService to fetch and display an investment

--- Daria_Deriagina_Learning_Angular/src/app/app.component.ts
+++ Daria_Deriagina_Learning_Angular/src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Investment } from './models/investment';
 import {InvestmentListComponent} from "./investment-list/investment-list.component";
+import { InvestmentListItemComponent } from './investment-list-item/investment-list-item.component';
+import { InvestmentService } from './services/investment.service'; // Step 11: Import InvestmentService
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,19 @@ import {InvestmentListComponent} from "./investment-list/investment-list.compone
   styleUrls: ['./app.component.scss'],
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Investment Calculator';
+  selectedInvestment: Investment | undefined; // Step 12: Use to store the investment retrieved by the service
+
+  // Step 11: Inject the InvestmentService into the constructor
+  constructor(private investmentService: InvestmentService) {}
+
+  // Step 12: Use ngOnInit to retrieve an investment item by ID
+  ngOnInit(): void {
+    this.investmentService.getInvestmentById(1).subscribe({
+      next: (data: Investment | undefined) => this.selectedInvestment = data, // Step 12: Assign the fetched investment to selectedInvestment
+      error: (err) => console.error('Error fetching investment', err),
+      complete: () => console.log('Investment data fetch complete!')
+    });
+  }
 }
