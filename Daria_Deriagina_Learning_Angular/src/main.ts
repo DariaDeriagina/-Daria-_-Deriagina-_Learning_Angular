1d7e4ddbc0a17583711afc23b382b593629b23a1
Daria Deriagina <w0862447@myscc.ca>
2024-11-07 14:13:29 -0500

    add data to the memory data service

--- Daria_Deriagina_Learning_Angular/src/main.ts
+++ Daria_Deriagina_Learning_Angular/src/main.ts
@@ -5,9 +5,14 @@ import { InvestmentListComponent } from "./app/investment-list/investment-list.c
 import { PageNotFoundComponent } from "./app/page-not-found/page-not-found.component";
 import { ModifyListItemComponent } from "./app/modify-list-item/modify-list-item.component";
 import {InvestmentListItemComponent} from "./app/investment-list-item/investment-list-item.component";
+import {provideHttpClient} from "@angular/common/http";
+import {importProvidersFrom} from "@angular/core";
+import {delay} from "rxjs";
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import {InMemoryDataService} from "./app/services/in-memory-data.service";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/investments', pathMatch: 'full'},
+  {path: '', redirectTo: '/w', pathMatch: 'full'},
   { path: 'investments', component: InvestmentListComponent },
   { path: 'investments/:id', component: InvestmentListItemComponent },
   {path: 'modify-investment', component: ModifyListItemComponent},
@@ -16,5 +21,8 @@ const routes: Routes = [
 ];
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
-}).then(r => console.log('Bootstrap successful'));
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+  importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {delay:1000}))],
+}).catch((err)=>console.error(err));
