f48940518e59d6c923330b95f46db8ad8202f014
Daria Deriagina <w0862447@myscc.ca>
2024-11-07 14:36:36 -0500

    implement CRUD operations

--- Daria_Deriagina_Learning_Angular/src/app/services/investment.service.ts
+++ Daria_Deriagina_Learning_Angular/src/app/services/investment.service.ts
@@ -1,62 +1,72 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import {catchError, Observable, of, throwError} from 'rxjs';
 import { Investment } from '../models/investment';
 import { investmentsList } from '../data/mock-investment';
 import { Router } from '@angular/router';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvestmentService {
+
+  private apiUrl = 'api/investments';
+  //local copy to investment list
   private investments = investmentsList;
 
-  constructor(private router: Router) {}
+  constructor(private http:HttpClient) {}
+
+
+  //CRUD
+  //All operations we need are:
+  //Get post put delete
+
 
   // Get all investments
   getInvestments(): Observable<Investment[]> {
-    return of(this.investments);
+    return this.http.get<Investment[]>(this.apiUrl).pipe(catchError(this.handleError));
   }
 
   // Get investment by ID
-  getInvestmentById(id: number): Observable<Investment | undefined> {
-    const investment = this.investments.find(investment => investment.id === id);
-    return of(investment);
+  getInvestmentById(id: number): Observable<Investment> {
+    return this.http.get<Investment>(`${this.apiUrl}/${id}`).pipe(catchError(this.handleError));
+
   }
 
   // Add a new investment
 
 
-  addInvestment(newInvestment: Investment): Observable<void> {
+  addInvestment(newInvestment: Investment): Observable<Investment> {
     newInvestment.id = this.generateNewId();
-    this.investments.push(newInvestment);
-    return of();
+    return this.http.post<Investment>(this.apiUrl, newInvestment).pipe(catchError(this.handleError));
+
 
   }
 
   // Update an existing investment
-  updateInvestment(updatedInvestment: Investment): Observable<void> {
-    const index = this.investments.findIndex(investment => investment.id === updatedInvestment.id);
-    if (index !== -1) {
-      this.investments[index] = updatedInvestment;
-    }
-    return of();
+  updateInvestment(updatedInvestment: Investment): Observable<Investment | undefined> {
+    const url = `${this.apiUrl}/${updatedInvestment.id}`;
+    return this.http.post<Investment>(url, updatedInvestment).pipe(catchError(this.handleError));
+
   }
 
   // Navigate to edit investment
-  editInvestment(id: number): void {
-    this.router.navigate(['/modify-investment', id]);
-  }
+  // editInvestment(id: number): void {
+  //   this.router.navigate(['/modify-investment', id]);
+  // }
 
   // Delete investment
-  deleteInvestment(id: number): Observable<void> {
-    const index = this.investments.findIndex(investment => investment.id === id);
-    if (index !== -1) {
-      this.investments.splice(index, 1);
-    }
-    return of();
+  deleteInvestment(id: number): Observable<{}> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url).pipe(catchError(this.handleError));
   }
 
   generateNewId(): number{
     return this.investments.length >0 ? Math.max(...this.investments.map(investment => investment.id)) +1 : 1;
+  }
+
+  private handleError(error: HttpErrorResponse){
+    console.error('API error', error);
+    return throwError(()=> new Error ('Server error, please try again'));
   }
 }
