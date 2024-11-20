81ed2e2d3bdca6d398b683833d6602165442a2bb
Daria Deriagina <w0862447@myscc.ca>
2024-11-20 17:14:06 -0500

    Assignment 9 finished

--- Daria_Deriagina_Learning_Angular/src/app/directives/show-details-on-hover.directive.ts
+++ Daria_Deriagina_Learning_Angular/src/app/directives/show-details-on-hover.directive.ts
@@ -14,23 +14,21 @@ export class ShowDetailsOnHoverDirective {
   @HostListener('mouseenter') onMouseEnter() {
     console.log('Hover started for: ', this.details);
     if (!this.tooltip) {
-      // Create tooltip element
       this.tooltip = this.renderer.createElement('span');
-      this.renderer.addClass(this.tooltip, 'tooltip'); // Optional: Add a tooltip class for styling
-      // this.renderer.setStyle(this.tooltip, 'position', 'absolute');
-      // this.renderer.setStyle(this.tooltip, 'background-color', '#333');
-      // this.renderer.setStyle(this.tooltip, 'color', '#fff');
-      // this.renderer.setStyle(this.tooltip, 'padding', '5px');
-      // this.renderer.setStyle(this.tooltip, 'border-radius', '3px');
-      // this.renderer.setStyle(this.tooltip, 'top', '-25px');
-      // this.renderer.setStyle(this.tooltip, 'left', '0');
-      // this.renderer.setStyle(this.tooltip, 'z-index', '1000');
-      // this.renderer.setStyle(this.tooltip, 'white-space', 'nowrap');
+      this.renderer.addClass(this.tooltip, 'tooltip');
       this.renderer.appendChild(this.tooltip, this.renderer.createText(this.details));
-      this.renderer.appendChild(this.el.nativeElement, this.tooltip);
-      console.log('Tooltip created:', this.tooltip); // Debugging
+      this.renderer.appendChild(document.body, this.tooltip);
+
+      const rect = this.el.nativeElement.getBoundingClientRect();
+      this.renderer.setStyle(this.tooltip, 'top', `${rect.top + window.scrollY - 30}px`);
+      this.renderer.setStyle(this.tooltip, 'left', `${rect.left + window.scrollX}px`);
+      // Add a delay for smooth fade-in
+      setTimeout(() => this.renderer.addClass(this.tooltip, 'show'), 10);
     }
+
+
   }
+
 
   @HostListener('mouseleave') onMouseLeave() {
     console.log('Hover ended for: ', this.details);
